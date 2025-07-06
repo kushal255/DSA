48e71da35cbd57d53473aab3a4f4227a5c25f3d1
Kushal Koradia <123183175+kushal255@users.noreply.github.com>
2025-07-06 23:22:47 +0530

    Add solution for 1137. N-th Tribonacci Number

---
+++ easy/1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
@@ -0,0 +1,14 @@
+          int secondTerm = 1;
+          int thirdTerm = 1;
+          for(int i =0; i<n; i++){
+            int fourthTerm = firstTerm + secondTerm + thirdTerm ;
+          int firstTerm = 0;
+          
+        return 1;
+      }else{
+      }else if(n==1 | n==1){
+            firstTerm = secondTerm;
+            secondTerm = thirdTerm;
+            thirdTerm = fourthTerm;
+          }
+      return firstTerm;  
